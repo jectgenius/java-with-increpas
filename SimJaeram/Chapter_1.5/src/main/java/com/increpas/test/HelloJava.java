9eec58b430f53fd7abf2702ed57a960d9157cb52
jectgenius <jkl9087@naver.com>
2024-07-08 22:56:46 +0900

    Comment: add comment Chpater1.5 개발환경구축 #23

--- SimJaeram/Chapter_1.5/src/main/java/com/increpas/test/HelloJava.java
+++ SimJaeram/Chapter_1.5/src/main/java/com/increpas/test/HelloJava.java
@@ -1,7 +1,7 @@
 package com.increpas.test;
 
-public class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("Hello? Java!! 안녕? 자바야!!");
-    }
-}
+public class HelloJava {                                    // HelloJava 클래스 정의 사적
+    public static void main(String[] args) {                // main() 메소드 정의 시작
+        System.out.println("Hello? Java!! 안녕? 자바야!!");   // 콘솔 화면에 문자열 출력하는 메소드 System.out.println() 호출하여, 문자열 "Hello? Java!! 안녕? 자바야!! 출력
+    }                                                       // main() 메소드 정의 끝
+}                                                           // HelloJava 클래스 정의 끝
